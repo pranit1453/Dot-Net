27c8bd8caaca694f7d6d65913da950d890473a39
pranit1453 <pranitbhangale1@gmail.com>
2025-12-29 23:21:26 +0530

    Entity Framework

---
+++ MyData/EntityFramework/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EntityFramework.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("Student_Name")]
+        public string? Name { get; set; }
+        [Column("Student_Age")]
+        public int Age { get; set; }
+    }
+}
